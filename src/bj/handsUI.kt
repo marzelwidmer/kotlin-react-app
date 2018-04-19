4493146fcffee60feb38afa1a83961228d70b6c2
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-19 23:27:13 +0200

    style

--- src/bj/handsUI.kt
+++ src/bj/handsUI.kt
@@ -1,11 +1,23 @@
 package bj
 
+import kotlinext.js.js
+import kotlinx.html.style
 import react.RBuilder
 import react.dom.b
 import react.dom.div
 
 fun RBuilder.handUI() {
     div {
+
+        attrs.style = js {
+            with = "10rem"
+            height = "10rem"
+            padding = "1rem"
+            marginTop = "1rem"
+            marginRight = "1rem"
+            background = "cyan"
+        }
+
         div { b { +"Player hand" } }
         div { +"cards goes here" }
         div { b { +"12 Points" } }
